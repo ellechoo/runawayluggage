c7dd3424895f53521a5acc7b4aff5b791b592b36
Elle Choo <michellehchoo@gmail.com>
2025-02-06 05:17:25 +0000

    begging

--- script.js
+++ script.js
@@ -1,5 +1,56 @@
+let manager = new THREE.LoadingManager();
 let typo = null;
 let particle = null; // Initialize the variable as null
+
+//moved environment
+class Environment {
+  constructor(font, particle) {
+      this.font = font;
+      this.particle = particle;
+      this.container = document.querySelector('#magic');
+      this.scene = new THREE.Scene();
+      this.createCamera();
+      this.createRenderer();
+      this.setup();
+      this.bindEvents();
+  }
+
+  bindEvents(){
+      window.addEventListener('resize', this.onWindowResize.bind(this));
+  }
+
+  setup() { 
+      this.createParticles = new CreateParticles(this.scene, this.font, this.particle, this.camera, this.renderer);
+  }
+
+  render() {
+      this.createParticles.render();
+      this.renderer.render(this.scene, this.camera);
+  }
+
+  createCamera() {
+      this.camera = new THREE.PerspectiveCamera(65, this.container.clientWidth / this.container.clientHeight, 1, 10000);
+      this.camera.position.set(0,0, 100);
+  }
+
+  createRenderer() {
+      this.renderer = new THREE.WebGLRenderer();
+      this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
+      this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+      this.renderer.outputEncoding = THREE.sRGBEncoding;
+      this.container.appendChild(this.renderer.domElement);
+      this.renderer.setAnimationLoop(() => { this.render() });
+  }
+
+  onWindowResize() {
+      this.camera.aspect = this.container.clientWidth / this.container.clientHeight;
+      this.camera.updateProjectionMatrix();
+      this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
+  }
+}
+
+
+
 
 // Error handling for texture loading
 new THREE.TextureLoader(manager)
@@ -55,51 +106,7 @@ if (document.readyState === "complete" || (document.readyState !== "loading" && 
     document.addEventListener("DOMContentLoaded", preload);
 }
 
-class Environment {
-    constructor(font, particle) {
-        this.font = font;
-        this.particle = particle;
-        this.container = document.querySelector('#magic');
-        this.scene = new THREE.Scene();
-        this.createCamera();
-        this.createRenderer();
-        this.setup();
-        this.bindEvents();
-    }
-
-    bindEvents(){
-        window.addEventListener('resize', this.onWindowResize.bind(this));
-    }
-
-    setup() { 
-        this.createParticles = new CreateParticles(this.scene, this.font, this.particle, this.camera, this.renderer);
-    }
-
-    render() {
-        this.createParticles.render();
-        this.renderer.render(this.scene, this.camera);
-    }
-
-    createCamera() {
-        this.camera = new THREE.PerspectiveCamera(65, this.container.clientWidth / this.container.clientHeight, 1, 10000);
-        this.camera.position.set(0,0, 100);
-    }
-
-    createRenderer() {
-        this.renderer = new THREE.WebGLRenderer();
-        this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
-        this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-        this.renderer.outputEncoding = THREE.sRGBEncoding;
-        this.container.appendChild(this.renderer.domElement);
-        this.renderer.setAnimationLoop(() => { this.render() });
-    }
-
-    onWindowResize() {
-        this.camera.aspect = this.container.clientWidth / this.container.clientHeight;
-        this.camera.updateProjectionMatrix();
-        this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
-    }
-}
+//old environment placement
 
   
   class CreateParticles {
