8a7b846c33a8a578d1a6743e299df7a40516992a
Elle Choo <michellehchoo@gmail.com>
2025-02-13 16:01:54 +0000

    color? blue

--- script.js
+++ script.js
@@ -163,7 +163,7 @@ const preload = () => {
                 let py = pos.getY(i);
                 let pz = pos.getZ(i);
     
-                this.colorChange.setHSL( .5, 1 , 1 )
+                this.colorChange.setHSL( 0.66 , 1.0 , 0.3 )
                 coulors.setXYZ( i, this.colorChange.r, this.colorChange.g, this.colorChange.b )
                 coulors.needsUpdate = true;
     
@@ -186,7 +186,7 @@ const preload = () => {
                         px -= .03 * Math.cos( t );
                         py -= .03 * Math.sin( t );
     
-                        this.colorChange.setHSL( .15 , 1.0 , .5 )
+                        this.colorChange.setHSL( 0.66 , 1.0 , 0.3 )
                         coulors.setXYZ( i, this.colorChange.r, this.colorChange.g, this.colorChange.b )
                         coulors.needsUpdate = true;
     
@@ -208,7 +208,7 @@ const preload = () => {
     
                     if ((px > (initX + 10)) || ( px < (initX - 10)) || (py > (initY + 10) || ( py < (initY - 10)))){
     
-                        this.colorChange.setHSL( .15, 1.0 , .5 )
+                        this.colorChange.setHSL( 0.66 , 1.0 , 0.3 )
                         coulors.setXYZ( i, this.colorChange.r, this.colorChange.g, this.colorChange.b )
                         coulors.needsUpdate = true;
     
