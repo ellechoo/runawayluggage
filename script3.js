d87f49303f685f8549f34cf9b022ec4b46ffabfe
Elle Choo <michellehchoo@gmail.com>
2025-02-13 16:22:24 +0000

    //

--- script3.js
+++ script3.js
@@ -208,7 +208,7 @@ const preload = () => {
     
                     if ((px > (initX + 10)) || ( px < (initX - 10)) || (py > (initY + 10) || ( py < (initY - 10)))){
     
-                        this.colorChange.setHSL( .15, 1.0 , .5 )
+                        this.colorChange.setHSL( 0.66 , 1.0 , 0.3 )
                         coulors.setXYZ( i, this.colorChange.r, this.colorChange.g, this.colorChange.b )
                         coulors.needsUpdate = true;
     
