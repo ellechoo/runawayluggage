8ab171a8f953985d17ef2bb1e850e79a86e80d14
Elle Choo <michellehchoo@gmail.com>
2025-02-16 23:19:39 +0000

    to the left?

--- script4.js
+++ script4.js
@@ -237,7 +237,7 @@ const preload = () => {
         let geometry = new THREE.ShapeGeometry( shapes );
         geometry.computeBoundingBox();
     
-        const xMid = - 0.5 * ( geometry.boundingBox.max.x - geometry.boundingBox.min.x );
+        const xMid = - 0.8 * ( geometry.boundingBox.max.x - geometry.boundingBox.min.x );
         const yMid =  (geometry.boundingBox.max.y - geometry.boundingBox.min.y)/2.85;
     
         geometry.center();
