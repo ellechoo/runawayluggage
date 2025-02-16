2107f2b9298fd07a4ac0401a45ffa8a23d95f987
Elle Choo <michellehchoo@gmail.com>
2025-02-16 23:37:08 +0000

    switch

--- script.js
+++ script.js
@@ -186,7 +186,7 @@ const preload = () => {
                         px -= .03 * Math.cos( t );
                         py -= .03 * Math.sin( t );
     
-                        this.colorChange.setHSL( 0.5 , 1.0 , 1.0 )
+                        this.colorChange.setHSL( 0.66 , 1.0 , 0.3 )
                         coulors.setXYZ( i, this.colorChange.r, this.colorChange.g, this.colorChange.b )
                         coulors.needsUpdate = true;
     
@@ -208,7 +208,7 @@ const preload = () => {
     
                     if ((px > (initX + 10)) || ( px < (initX - 10)) || (py > (initY + 10) || ( py < (initY - 10)))){
     
-                        this.colorChange.setHSL( 0.66 , 1.0 , 0.3 )
+                        this.colorChange.setHSL( 0.5 , 1.0 , 1.0 )
                         coulors.setXYZ( i, this.colorChange.r, this.colorChange.g, this.colorChange.b )
                         coulors.needsUpdate = true;
     
